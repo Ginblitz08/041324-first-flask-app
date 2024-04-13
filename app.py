aab1ea124481bdcd56e639717f22bc3a4ea1db57
Gino Villarico <ginblitz06@gmail.com>
2024-04-13 15:09:08 +0800

    html templates implementation

--- app.py
+++ app.py
@@ -1,17 +1,45 @@
-# from flask import Flask
+# from flask import Flask                  from here
 
-# app=Flask(__name__)
+# app=Flask(__name__)                       basic format:    
 # if __name__== "__main__":
-#     app.run()
+#     app.run()                             to here
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route("/", methods=["GET"])                    #-decorator
+#@app.route("/", methods=["GET"])                    #-decorator      http://127.0.0.1:5000 in browser
+#def hello_world():
+#   return "Hello, World!"
+
+@app.route("/", methods=["GET"])                    #-decorator      http://127.0.0.1:5000 in browser
 def hello_world():
-    return "Hello, World!"
+    html="""
+    <html>
+        <head>
+            <title>Hello, World!</title>
+        </head>
+        <body>
+            <h1>Hello, World!</h1>
+            <p>This app is written using Flask</p>
+        </body>
+    </html>
+    """
+    return html
+    #return "Hello, World!"
+
+# @app.route("/about", methods=["GET"])               #     http://127.0.0.1:5000/about in browser
+# def about():
+#     return "this app is created by Gino"
+
+@app.route("/about", methods=["GET"])             
+def about():
+    return render_template("about.html")
+
+@app.route("/contact",methods=["GET"])              #     http://127.0.0.1:5000/contact in browser
+def contact():
+    return "this is the contact page"
 
 if __name__== "__main__":
     app.run()
